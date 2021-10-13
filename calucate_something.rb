5c3652690d8851dc73515e164542581f67b064f3
Garllon <palluthe.bennet@gmail.com>
2021-10-13 09:46:43 +0200

    add first round of creating code

---
+++ calucate_something.rb
@@ -0,0 +1,23 @@
+puts "Hallo lieber Nutzer"
+
+next_run = true
+
+while(next_run) do
+  puts "Wie hoch ist deine monatliche Miete in €?"
+  miete = gets.to_i
+
+  puts "Wie teuer ist dein Auto?"
+  auto = gets.to_i
+
+  puts "Wie hoch sind deine Lebenshaltungskosten?"
+  lhkosten = gets.to_i
+
+  result = miete+auto+lhkosten
+  puts "So viel von deinem Gehalt hast du nicht zur freien Verfügung:"
+  puts result
+  puts ":("
+
+  puts "Willst du weiter machen? Dann schreibe 'ja'"
+  input = gets
+  next_run = input == "ja"
+end
